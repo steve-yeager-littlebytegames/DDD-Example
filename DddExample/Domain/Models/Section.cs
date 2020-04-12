4644146c81a06644e0068dac7545036f252a8093
Steve Yeager <steve.yeager@littlebytegames.com>
2020-04-11 22:19:53 -0700

    Add SectionValidator

--- DddExample/Domain/Models/Section.cs
+++ DddExample/Domain/Models/Section.cs
@@ -1,12 +1,31 @@
-﻿namespace DddExample.Domain.Models
+﻿using DddExample.Domain.Validation;
+using FluentValidation;
+
+namespace DddExample.Domain.Models
 {
+    public class SectionValidator : AbstractValidator<Section>
+    {
+        public SectionValidator()
+        {
+            RuleFor(s => s.Name).Length(1, 50);
+        }
+    }
+
     public class Section
     {
         public string Name { get; }
 
-        public Section(string name)
+        private Section(string name)
         {
             Name = name;
+        }
+
+        public static Result<Section> Construct(string name, SectionValidator validator)
+        {
+            var section = new Section(name);
+
+            var result = validator.Validate(section);
+            return Result<Section>.Construct(section, result);
         }
     }
 }
